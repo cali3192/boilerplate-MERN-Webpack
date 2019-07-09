294a526cd2b5d97ef2556c3b6d07ce3643cc60bf
Cali Tabilas <ctabilas23@gmail.com>
2019-07-09 15:13:04 -0400

    fixed config notes

--- webpack.config.js
+++ webpack.config.js
@@ -1,33 +1,11 @@
-// You shouldn't have to touch this webpack file.
+const webpack = require('webpack');
+const path = require('path');
 
-// IF WEBPACK IS NOT WORKING
-// and you are getting error messages indicating an incompatibility in versions,
-// make sure you are running a version of webpack which is compatible with
-// this configuration file (e.g. 2.3)
-// Install such a version of webpack using this command:
-// npm install -g webpack@2.3
+const BUILD_DIR = path.resolve(__dirname, 'client/dist');
 
-// Webpack is a module bundler, which means it takes modules with dependencies
-// and packages them into one bundle file. In this configuration, it also uses
-// babel to transpile the files before bundling. Webpack knows which files to include
-// by starting with the 'entry' file in the config, and following the es6 import
-// statements.
-var webpack = require('webpack');
-var path = require('path');
+const APP_DIR = path.resolve(__dirname, 'client/src');
 
-// Build directory is where the bundle file will be placed
-var BUILD_DIR = path.resolve(__dirname, 'client/dist');
-// App directory is where all of your raw JSX files will be placed
-var APP_DIR = path.resolve(__dirname, 'client/src');
-
-// The files in the app directory will get transpiled and packaged into one
-// file, bundle.js, which will get saved in the BUILD_DIR.
-// If you use the `npm run dev-react`, webpack will generate source maps and
-// watch your files for changes.
-
-// While developing your app in react, you'll want to have two terminal tabs open -
-// one that is running `npm run dev-react` and one that is running `npm start`
-var config = {
+const config = {
   entry: APP_DIR + '/index.jsx',
   module: {
     loaders: [
